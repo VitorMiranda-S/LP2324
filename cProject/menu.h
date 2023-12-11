deae41266a106c54d4bf036d31bfdefe39c6c452
VitorMiranda-S <vitorsmiranda99@gmail.com>
2023-12-11 10:07:17 +0000

    Second

---
+++ cProject/menu.h
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file_header.h to edit this template
+ */
+
+/* 
+ * File:   menu.h
+ * Author: vitor
+ *
+ * Created on 11 de dezembro de 2023, 09:57
+ */
+
+#ifndef MENU_H
+#define MENU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void menuInicial();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MENU_H */
