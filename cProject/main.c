5479f0d67aecff0145e908e8d3ef99e903c48a2b
VitorMiranda-S <vitorsmiranda99@gmail.com>
2023-12-11 10:25:19 +0000

    KJH

---
+++ cProject/main.c
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: vitor
+ *
+ * Created on 11 de dezembro de 2023, 09:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    LJHJKHJKH
+    return (EXIT_SUCCESS);
+}
+
